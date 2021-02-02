6e804625a410a49329ca3cec15370dcc42931c33
Rafael Veronezi <ravero@outlook.com>
2021-02-02 11:04:28 -0300

    Update env-bootstrap.sh

--- env-bootstrap.sh
+++ env-bootstrap.sh
@@ -3,14 +3,18 @@
 # Environment Bootstrapper
 #------------------------------------------------------------------------------
 
-## Homebrew Installation
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+## Homebrew Installation/Update
+if brew ls --versions myformula > /dev/null; then
+    brew update
+else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 ## Clone the Environment Repository
 ENVIRONMENT_GIT_REMOTE="https://github.com/ravero/environment.git"
 ENVIRONMENT_LOCAL_PATH="${HOME}/Environment"
 
 # Check if the Environment Folder doesn't already exists
-if [[ !-d $ENVIRONMENT_LOCAL_PATH ]]; then
+if [[ ! -d $ENVIRONMENT_LOCAL_PATH ]]; then
     git clone $ENVIRONMENT_GIT_REMOTE "${ENVIRONMENT_LOCAL_PATH}"
 fi
